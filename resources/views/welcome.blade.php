8dbdefa059021b899f0672ae1d257e456e244515
BayMx19 <isa.20019@mhs.unesa.ac.id>
2023-05-22 21:18:40 +0700

    Update Dashboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -462,7 +462,7 @@
     <main>
         <nav class="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
             <div class="container">
-                <a class="navbar-brand fw-bold" href="#page-top">
+                <a class="navbar-brand fw-bold" href="{{ '' }}">
                     <img src="img/Logo-panjang.png">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
