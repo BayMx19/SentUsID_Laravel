37b551c1981d086103188f5ee60b1d6f4e2ca0da
BayMx19 <isa.20019@mhs.unesa.ac.id>
2023-05-25 15:15:58 +0700

    Update - Add Pages for Users

---
+++ resources/views/list-pengguna/addusers.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('title', 'Add Users')
+
+@section('content')
+    <div class="main-content">
+        <div class="main-content-inner">
+
+        </div>
+    </div>
+@endsection
---
+++ resources/views/list-pengguna/detailusers.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('title', 'Detail Users')
+
+@section('content')
+    <div class="main-content">
+        <div class="main-content-inner">
+
+        </div>
+    </div>
+@endsection
