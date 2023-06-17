f4054c348545339b2706548866e4f0ab4ab7f8ff
BayMx19 <isa.20019@mhs.unesa.ac.id>
2023-06-17 14:09:20 +0700

    Update - Produk

--- resources/views/list-produk/produk.blade.php
+++ resources/views/list-produk/produk.blade.php
@@ -20,27 +20,41 @@
                             <div class="row mt-5 mb-3">
                                 <div class="col-sm-4">
                                     <div class="card" style="width: 18rem;">
-                                        <img src="..." class="card-img-top" alt="...">
-                                        <div class="card-body">
+                                        <div class="card-body card-produk">
+                                            <img src="img/tes.JPG" class="card-img-top" alt="...">
                                             <p class="card-text">Some quick example text to build on the card title and make
                                                 up the bulk of the card's content.</p>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="col-sm-4">
-                                    <div class="card">
-                                        <div class="card-produk">
-
+                                    <div class="card" style="width: 18rem;">
+                                        <div class="card-body card-produk">
+                                            <img src="img/tes.JPG" class="card-img-top" alt="...">
+                                            <p class="card-text">Some quick example text to build on the card title and make
+                                                up the bulk of the card's content.</p>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="col-sm-4">
-                                    <div class="card">
-                                        <div class="card-produk">
-
+                                    <div class="card" style="width: 18rem;">
+                                        <div class="card-body card-produk">
+                                            <img src="img/tes.JPG" class="card-img-top" alt="...">
+                                            <p class="card-text">Some quick example text to build on the card title and make
+                                                up the bulk of the card's content.</p>
                                         </div>
                                     </div>
                                 </div>
+                                <div class="col-sm-4">
+                                    <div class="card" style="width: 18rem;">
+                                        <div class="card-body card-produk">
+                                            <img src="img/tes.JPG" class="card-img-top" alt="...">
+                                            <p class="card-text">Some quick example text to build on the card title and make
+                                                up the bulk of the card's content.</p>
+                                        </div>
+                                    </div>
+                                </div>
+
 
 
 
